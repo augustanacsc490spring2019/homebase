9ca41195df34870201c02df3aaaa22fe33f57383
Elliyos <k.mccaw25@gmail.com>
2019-03-22 16:54:28 -0500

    Removed last signs of the ancient one

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,10 +39,9 @@ function Footer({ ...props }) {
         <p className={classes.right}>
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
-            <a href="https://www.creative-tim.com" className={classes.a}>
-              Creative Tim
+            <a href="#" className={classes.a}>
+              homebase
             </a>
-            , made with love for a better web
           </span>
         </p>
       </div>
