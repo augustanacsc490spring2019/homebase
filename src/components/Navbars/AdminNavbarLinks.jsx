502705c393156950cd1b74e1c1ebafc862acaf68
Gearen <liemgearen15@augustana.edu>
2019-05-12 13:23:27 -0500

    removed unnecessary imports

--- src/components/Navbars/AdminNavbarLinks.jsx
+++ src/components/Navbars/AdminNavbarLinks.jsx
@@ -1,28 +1,17 @@
 import React from "react";
-import classNames from "classnames";
 import PropTypes from "prop-types";
 import { compose } from "redux";
 import { connect } from "react-redux";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
-import Grow from "@material-ui/core/Grow";
-import Paper from "@material-ui/core/Paper";
-import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import Hidden from "@material-ui/core/Hidden";
-import Poppers from "@material-ui/core/Popper";
 // @material-ui/icons
 import Person from "@material-ui/icons/Person";
-import Notifications from "@material-ui/icons/Notifications";
-import Dashboard from "@material-ui/icons/Dashboard";
-import Search from "@material-ui/icons/Search";
 
 import { Link } from "react-router-dom";
 // core components
 
 import firebase from "../../reference/firebase";
-import CustomInput from "components/CustomInput/CustomInput.jsx";
 import Button from "components/CustomButtons/Button.jsx";
 
 import headerLinksStyle from "assets/jss/material-dashboard-react/components/headerLinksStyle.jsx";
@@ -56,7 +45,6 @@ class HeaderLinks extends React.Component {
 
   render() {
     const { classes } = this.props;
-    const { open } = this.state;
     return (
       <div>
         <Button
