3a4928d82d393c273ecd1369cea1cf98a640bc34
Elliyos <k.mccaw25@gmail.com>
2019-05-14 01:01:13 -0500

    Working MapAutocomplete w/ async

--- src/components/MapAutocomplete.js
+++ src/components/MapAutocomplete.js
@@ -8,7 +8,39 @@ import PlacesAutocomplete, {
 export default class LocationSearchInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { address: "", id: this.props.id };
+    this.state = { address: "", id: this.props.id, gmapsLoaded: false };
+  }
+
+  initAC = () => {
+    this.setState({
+      gmapsLoaded: true
+    });
+  };
+
+  componentDidMount() {
+    window.initAC = this.initAC;
+    const gmapScriptEl = document.createElement(`script`);
+    gmapScriptEl.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBNA4QW8E2kzYU_6eR7QmikowbqB3EupJc&libraries=places&callback=initAC`;
+    document
+      .querySelector(`body`)
+      .insertAdjacentElement(`beforeend`, gmapScriptEl);
+  }
+
+  componentWillUnmount() {
+    // remove all 'maps.googleapis.com' scripts injected into the document
+    // https://stackoverflow.com/a/9469983/5045662
+    const tags = document.getElementsByTagName("script");
+    for (let i = tags.length; i >= 0; i--) {
+      //search backwards within nodelist for matching elements to remove
+      if (
+        tags[i] &&
+        tags[i].getAttribute("src") != null &&
+        tags[i].getAttribute("src").includes("maps.googleapis.com")
+      )
+        tags[i].parentNode.removeChild(tags[i]); //remove element by calling parentNode.removeChild()
+    }
+    // set the google.maps instance on the window to null
+    window.google.maps = null;
   }
 
   handleChange = address => {
@@ -26,44 +58,48 @@ export default class LocationSearchInput extends React.Component {
 
   render() {
     return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <TextField
-              {...getInputProps({
-                placeholder: "Search Places ...",
-                className: "location-search-input",
-                id: this.props.id
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {suggestions.map(suggestion => {
-                const className = suggestion.active
-                  ? "suggestion-item--active"
-                  : "suggestion-item";
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                  : { backgroundColor: "#ffffff", cursor: "pointer" };
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style
-                    })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+      <div>
+        {this.state.gmapsLoaded && (
+          <PlacesAutocomplete
+            value={this.state.address}
+            onChange={this.handleChange}
+            onSelect={this.handleSelect}
+          >
+            {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+              <div>
+                <TextField
+                  {...getInputProps({
+                    placeholder: "Search Places ...",
+                    className: "location-search-input",
+                    id: this.props.id
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? "suggestion-item--active"
+                      : "suggestion-item";
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                      : { backgroundColor: "#ffffff", cursor: "pointer" };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
         )}
-      </PlacesAutocomplete>
+      </div>
     );
   }
 }
